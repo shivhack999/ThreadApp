1e1ec37de796d66ad39c1286652291d7671e8911
shiv <shivhack999@gmail.com>
2024-12-20 15:19:34 +0530

    a

--- middleware/security/identifyDevice.js
+++ middleware/security/identifyDevice.js
@@ -1,13 +1,18 @@
 const useragent = require('useragent');
-const deviceIdentify = async(req, res, next) =>{
+
+const deviceIdentify = async (req, res, next) => {
   const agent = useragent.parse(req.headers['user-agent']);
-    if (agent.device.family === 'Other') {
-        req.device = "Web";
-        console.log("web")
-    } else {
-        req.device = "App";
-        console.log("App")
-    }
+  console.log('Parsed User-Agent:', agent);
+
+  if (agent.device.family === 'Other') {
+    req.device = "Web";
+    console.log("Identified as Web");
+  } else {
+    req.device = "App";
+    console.log("Identified as App");
+  }
+
   next();
-}
+};
+
 module.exports = deviceIdentify;
