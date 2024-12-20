1e2fc8587365b1a5c5105b4760fd9431513961a3
shiv <shivhack999@gmail.com>
2024-12-20 15:29:54 +0530

    a

--- middleware/security/identifyDevice.js
+++ middleware/security/identifyDevice.js
@@ -4,14 +4,20 @@ const deviceIdentify = async (req, res, next) => {
   const agent = useragent.parse(req.headers['user-agent']);
   console.log('Parsed User-Agent:', agent);
 
-  if (agent.device.family === 'Other') {
+  // Explicitly check for known cases
+  if (agent.source.includes('PostmanRuntime')) {
+    req.device = "Postman";
+    console.log("Identified as Postman");
+  } else if (agent.family === 'okhttp') {
+    req.device = "React Native App";
+    console.log("Identified as React Native App");
+  } else if (agent.device.family === 'Other') {
     req.device = "Web";
     console.log("Identified as Web");
   } else {
     req.device = "App";
     console.log("Identified as App");
   }
-
   next();
 };
 
